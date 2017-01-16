1e0a7dd783ee4c192696f78c54a884f3b1030ddf
stkim123 <stkim@jbnu.ac.kr>
2017-01-16 17:01:13 +0900

    Control Flow Check(Rule 14.*)-Test Suite

---
+++ test/test/kr/ac/jbnu/ssel/misrac/rule/TestRules14_ControlFlows.java
@@ -0,0 +1,13 @@
+package test.kr.ac.jbnu.ssel.misrac.rule;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+{ TestRule14_4.class, TestRule14_5.class, TestRule14_7.class, TestRule14_8.class })
+public class TestRules14_ControlFlows
+{
+
+}
