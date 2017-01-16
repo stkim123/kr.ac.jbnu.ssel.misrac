dd3d2aac9c8042769903de4cee69256141dfd1ae
sangjin9301 <sangjin9301@gmail.com>
2017-01-16 16:58:34 +0900

    Rule05_3 add comment and complete Junit test.

--- src/kr/ac/jbnu/ssel/misrac/rule/Rule05_3_Req.java
+++ src/kr/ac/jbnu/ssel/misrac/rule/Rule05_3_Req.java
@@ -10,6 +10,16 @@ import kr.ac.jbnu.ssel.misrac.rulesupport.AbstractMisraCRule;
 import kr.ac.jbnu.ssel.misrac.rulesupport.MessageFactory;
 import kr.ac.jbnu.ssel.misrac.rulesupport.ViolationMessage;
 
+/**
+ * A typedef name shall be a unique identifier.
+ *
+ * No typedef name shall be reused either as a typedef name or for any other purpose.
+ * 
+ * DONE!!
+ * 
+ * @author sangjin
+ *
+ */
 public class Rule05_3_Req extends AbstractMisraCRule {
 	private static HashSet<String> declarations = new HashSet<String>();
 
@@ -25,15 +35,16 @@ public class Rule05_3_Req extends AbstractMisraCRule {
 			String temp = node.getRawSignature();
 
 			if (declarations.contains(temp)) {
+//				The identifier '%1s' is declared as a typedef and is used elsewhere for a different kind of declaration.
 				String message1 = MessageFactory.getInstance().getMessage(1506);
 				violationMsgs.add(new ViolationMessage(this, getRuleID() + ":" + message1 + "--" + temp, node));
-
+//				'%1s' is used as a typedef for different types.
 				String message2 = MessageFactory.getInstance().getMessage(1507);
 				violationMsgs.add(new ViolationMessage(this, getRuleID() + ":" + message2 + "--" + temp, node));
-
+//				The typedef '%1s' is declared in more than one location.
 				String message3 = MessageFactory.getInstance().getMessage(1508);
 				violationMsgs.add(new ViolationMessage(this, getRuleID() + ":" + message3 + "--" + temp, node));
-
+//				Declaration of typedef '%s' is not in a header file although it is used in a definition or declaration with external linkage.
 				String message4 = MessageFactory.getInstance().getMessage(3448);
 				violationMsgs.add(new ViolationMessage(this, getRuleID() + ":" + message4 + "--" + temp, node));
 
