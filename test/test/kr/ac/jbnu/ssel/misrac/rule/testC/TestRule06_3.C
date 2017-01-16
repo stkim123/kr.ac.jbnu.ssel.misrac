3fb045bb9c2d64d321fb6986b6f775375c699203
stkim123 <stkim@jbnu.ac.kr>
2017-01-16 17:17:44 +0900

    test case for rule 6.3

---
+++ test/test/kr/ac/jbnu/ssel/misrac/rule/testC/TestRule06_3.C
@@ -0,0 +1,32 @@
+/*
+ * Rule6_3.C
+ *
+ *  Created on: 2017. 1. 13.
+ *      Author: stkim
+ */
+
+
+/*
+ * Rule6_3.C
+ *
+ *  Created on: 2017. 1. 13.
+ *      Author: stkim
+ */
+
+
+typedef          char   char_t;
+typedef signed   char   int8_t;
+typedef signed   short  int16_t;
+typedef signed   int    int32_t;
+typedef signed   long   int64_t;
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+typedef unsigned long   uint64_t;
+typedef          float  float32_t;
+typedef          double float64_t;
+typedef long     double float128_t;
+
+typedef String 	String128_t;	// violation
+typedef String 	String_t;	// not-violation
+
