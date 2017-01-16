c9bb82f053c141942b627fe9aef867eb828d9d9f
sangjin9301 <sangjin9301@gmail.com>
2017-01-16 15:44:31 +0900

    Rule20_8 add comment and complete Junit test

--- src/kr/ac/jbnu/ssel/misrac/rule/Rule20_8_Req.java
+++ src/kr/ac/jbnu/ssel/misrac/rule/Rule20_8_Req.java
@@ -8,6 +8,17 @@ import kr.ac.jbnu.ssel.misrac.rulesupport.AbstractMisraCRule;
 import kr.ac.jbnu.ssel.misrac.rulesupport.MessageFactory;
 import kr.ac.jbnu.ssel.misrac.rulesupport.ViolationMessage;
 
+
+/**
+ * The signal handling facilities of <signal.h> shall not be used.
+ *
+ * Signal handling contains implementation-defined and undefined behaviour.
+ *
+ * DONE!!
+ * @author stkim
+ *
+ */
+
 public class Rule20_8_Req extends AbstractMisraCRule{
 	
 	 private final static String Signal_H = "#include <signal.h>";
@@ -20,8 +31,9 @@ public class Rule20_8_Req extends AbstractMisraCRule{
 	   @Override
 	   protected int visit(IASTPreprocessorIncludeStatement includeStatement) {
 
-//	      System.out.println("test :: "+includeStatement.getRawSignature());
 	      if(includeStatement.getRawSignature().equals(Signal_H)){
+	    	  
+//	    	  The signal handling facilities of <signal.h> shall not be used.
 	         String message1 = MessageFactory.getInstance().getMessage(5123);
 	         violationMsgs.add(
 	               new ViolationMessage(this, getRuleID() + ":" + message1 + "--" + Signal_H, includeStatement));
