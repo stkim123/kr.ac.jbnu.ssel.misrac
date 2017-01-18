10282c78e9bf392733508c7dd17bd2dc1f9f7174
Kang seunghyeon <cat1919@naver.com>
2017-01-18 12:50:06 +0900

    Test Complete Rule18.4

---
+++ test/test/kr/ac/jbnu/ssel/misrac/rule/TestRule18_4.java
@@ -0,0 +1,40 @@
+package test.kr.ac.jbnu.ssel.misrac.rule;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import kr.ac.jbnu.ssel.misrac.rulesupport.ViolationMessage;
+import test.kr.ac.jbnu.ssel.misrac.rule.testsupport.AbstractTestRule;
+
+/**
+ * Sample Implementation for testing misra-c rule that has been implemented
+ * without executing Eclipse CDT.
+ * 
+ * DONE!!
+ * 
+ * @author kang
+ *
+ */
+
+public class TestRule18_4 extends AbstractTestRule {
+	public TestRule18_4() {
+		super("Rule18_4_Req", "TestRule18_4.C");
+	}
+
+	@Test
+	public void test() {
+		try {
+			ArrayList<ViolationMessage> violationMsgs = checkRule();
+			System.out.println(violationMsgs);
+			System.out.println("size:" + violationMsgs.size());
+
+			assertTrue(violationMsgs.size() == 4);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
