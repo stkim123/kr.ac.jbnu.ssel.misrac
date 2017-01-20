2bdb69dd3dc40838895806f0279f6253dac3d43b
SuntaeKim <stkim@jbnu.ac.kr>
2017-01-20 13:58:32 +0900

    add test case

---
+++ test/test/kr/ac/jbnu/ssel/misrac/rule/testC/TestRule02_2.C
@@ -0,0 +1,22 @@
+/*
+ * Rule02_2.c
+ *
+ *  Created on: 2016. 7. 22.
+ *      Author: STKim2
+ */
+
+
+#include "m2cmex.h"
+
+extern S16 test_0202( void )
+{
+   /* Only use C style comments */
+
+   s16a = 5;
+
+   // Do not use C++ comments
+
+   s16b = 6;
+
+   return 1;
+}
