64a2fbfba2fe7c15dad487672312571c2bb7ab91
default <default@default-PC>
2017-02-13 15:22:58 +0900

    Developer: ZzilLongLee
    Context of modifying: add two views(preference page, Table view)
    
    To do: need to integrate with original dataSet

---
+++ src/kr/ac/jbnu/ssel/castparser/view/Constant.java
@@ -0,0 +1,11 @@
+package kr.ac.jbnu.ssel.castparser.view;
+
+public class Constant {
+
+	//MISRA-C Table View Column Name
+	public static final String ruleNum = "Rules Num";
+	public static final String line = "Line";
+	public static final String errorCode = "Error Code";
+	public static final String errorMessage = "Error Message";
+	
+}
---
+++ src/kr/ac/jbnu/ssel/castparser/view/MisraTableView.java
@@ -0,0 +1,217 @@
+package kr.ac.jbnu.ssel.castparser.view;
+
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.*;
+
+import kr.ac.jbnu.ssel.castparser.view.TableViewColumns.MisraTableViewErrorCodeColumn;
+import kr.ac.jbnu.ssel.castparser.view.TableViewColumns.MisraTableViewErrorMSGColumn;
+import kr.ac.jbnu.ssel.castparser.view.TableViewColumns.MisraTableViewLineColumn;
+import kr.ac.jbnu.ssel.castparser.view.TableViewColumns.MisraTableViewRuleNumColumn;
+
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.graphics.Image;
+
+import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceProxy;
+import org.eclipse.core.resources.IResourceProxyVisitor;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourceAttributes;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.*;
+import org.eclipse.ui.internal.layout.Row;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+
+
+/**
+ * This sample class demonstrates how to plug-in a new
+ * workbench view. The view shows data obtained from the
+ * model. The sample creates a dummy model on the fly,
+ * but a real implementation would connect to the model
+ * available either in this or another plug-in (e.g. the workspace).
+ * The view is connected to the model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model
+ * objects should be presented in the view. Each
+ * view can present the same model objects using
+ * different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views
+ * in order to ensure that objects of the same type are
+ * presented in the same way everywhere.
+ * <p>
+ */
+
+public class MisraTableView extends ViewPart {
+
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "test.ui.views.MisraTableView";
+
+	private TableViewer viewer;
+	private TableViewer tableViewer;
+	private Action action1;
+	private Action action2;
+	private Action doubleClickAction;
+	
+
+	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
+		public String getColumnText(Object obj, int index) {
+			return getText(obj);
+		}
+		public Image getColumnImage(Object obj, int index) {
+			return getImage(obj);
+		}
+		public Image getImage(Object obj) {
+			return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+		}
+	}
+
+/**
+	 * The constructor.
+	 */
+	public MisraTableView() {
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		tableViewer = new TableViewer(parent, SWT.H_SCROLL|SWT.V_SCROLL);
+		tableViewer.getTable().setHeaderVisible(true);
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		// should make arrayType data;
+		String[] ids = TimeZone.getAvailableIDs();
+		TimeZone[] timeZones = new TimeZone[ids.length];
+		for (int i = 0; i < ids.length; i++) {
+			timeZones[i] = TimeZone.getTimeZone(ids[i]);
+		}
+		// should add some ColumInstance at here
+		 TableViewerColumn column = new MisraTableViewRuleNumColumn().addColumnTo(tableViewer);
+		new MisraTableViewLineColumn().addColumnTo(tableViewer);
+		new MisraTableViewErrorCodeColumn().addColumnTo(tableViewer);
+		new MisraTableViewErrorMSGColumn().addColumnTo(tableViewer);
+		
+		tableViewer.setInput(timeZones);
+		tableViewer.addDoubleClickListener(new IDoubleClickListener() {
+			
+			@Override
+			public void doubleClick(DoubleClickEvent e) {
+				IStructuredSelection selection = (IStructuredSelection)e.getSelection();
+				Row rowData = (Row)selection.getFirstElement();
+				System.out.println(rowData.toString());
+			}
+		});
+		
+		getSite().setSelectionProvider(tableViewer);
+		
+	}
+
+
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				MisraTableView.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(viewer.getControl());
+		viewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, viewer);
+	}
+
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(IMenuManager manager) {
+		manager.add(action1);
+		manager.add(new Separator());
+		manager.add(action2);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		manager.add(action1);
+		manager.add(action2);
+		// Other plug-ins can contribute there actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+	
+	private void fillLocalToolBar(IToolBarManager manager) {
+		manager.add(action1);
+		manager.add(action2);
+	}
+
+	private void makeActions() {
+		action1 = new Action() {
+			public void run() {
+				showMessage("Action 1 executed");
+			}
+		};
+		action1.setText("Action 1");
+		action1.setToolTipText("Action 1 tooltip");
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		
+		action2 = new Action() {
+			public void run() {
+				showMessage("Action 2 executed");
+			}
+		};
+		action2.setText("Action 2");
+		action2.setToolTipText("Action 2 tooltip");
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		doubleClickAction = new Action() {
+			public void run() {
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection)selection).getFirstElement();
+				showMessage("Double-click detected on "+obj.toString());
+			}
+		};
+	}
+
+	private void hookDoubleClickAction() {
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent event) {
+				doubleClickAction.run();
+			}
+		});
+	}
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			viewer.getControl().getShell(),
+			"aa View",
+			message);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		tableViewer.getControl().setFocus();
+	}
+	
+}
